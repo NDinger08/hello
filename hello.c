b14aa86a8f292afd227730304349f2c382f8225d
Nicholas <ndinger@huskers.unl.edu>
2026-08-27 17:28:38 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Nicholas Dinger
+ *         ndinger2@huskers.unl.edu
+ * Date: 8/27/2026
+ *
+ * A simple program that prints my name
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("My name is Nicholas Dinger\n");
+
+  return 0;
+}
